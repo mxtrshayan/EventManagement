c9a332ab4c4af6b62209bc7fd37efd23d1719b8c
Shayan <boloshayan@gmail.com>
2024-05-30 20:38:52 +0500

    Constraints are added

--- Tasks.sql
+++ Tasks.sql
@@ -1,15 +1,15 @@
-CREATE TABLE Tasks (
-    task_id INT PRIMARY KEY,
-    event_id INT,
-    task_description TEXT,
-    assigned_to VARCHAR(255),
-    status VARCHAR(50),
-    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+create table Tasks (
+    Task_id int primary key auto_increment,
+    Event_id int not null,
+    Task_description text not null,
+    Assigned_to varchar(55) not null,
+    Status varchar(50) not null,
+    foreign key (Event_id) references events(event_id)
 );
 
-insert into Tasks(task_id, event_id, task_description, assigned_to, status)
-values(1, 1, "To manage coming guests", "Sohan", "ready"),
-(2,1,'To serve dinner', 'Shahan', "ready"),
-(3,2,'Managment of hall', 'Maaz', 'Absent'),
-(4,3, 'To Communicate with nikkah khuwan','Farooq', 'Done'),
-(5,4, 'To manage sound system in venue', 'maaz', 'Ok');
+insert into tasks (event_id, task_description, assigned_to, status) values
+(1, 'To manage coming guests', 'Sohan', 'ready'),
+(1, 'To serve dinner', 'Shahan', 'ready'),
+(2, 'Management of hall', 'Maaz', 'Absent'),
+(3, 'To Communicate with nikkah khuwan', 'Farooq', 'Done'),
+(4, 'To manage sound system in venue', 'Maaz', 'Ok');
