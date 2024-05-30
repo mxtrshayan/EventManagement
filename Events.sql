fee93d6900ff546b1dc1cdba82a32b0ebbe4463e
Shayan <boloshayan@gmail.com>
2024-05-30 20:32:49 +0500

    Constraints are added

--- Events.sql
+++ Events.sql
@@ -1,15 +1,16 @@
-CREATE TABLE Events (
-    event_id INT PRIMARY KEY,
-    event_name VARCHAR(255),
-    event_type_id INT,
-    date DATE,
-    location VARCHAR(255),
-    description TEXT,
-    FOREIGN KEY (event_type_id) REFERENCES EventTypes(event_type_id)
+create table Events (
+    Event_id int primary key auto_increment,
+    Event_name varchar(50) not null,
+    Event_type_id int not null,
+    Date date not null,
+    Location varchar(55) not null,
+    Description text,
+    foreign key (Event_type_id) references eventtypes(event_type_id)
 );
 
-Insert into Events values (1,"Shayan's Engagement", 1, '2024-2-12', "Shahra e faisal karachi", "Event of our owner" ),
-(2,"Shayan's Marriage", 1, '2024-4-22', "surjani town", "Event of our owner" ),
-(3,"Ali's Nikkah", 1, '2024-8-11', "Memon masjid", "Event of our best client" ),
-(4, "AI Seminar", 2, '2024-08-13', 'JDC IT City', 'Big names will be in event'),
-(5, "English Language Course", 2, '2024-08-14', 'PIDC', 'Nothing special');
+insert into events ( event_name, event_type_id, date, location, description) values
+( "Shayan's Engagement", 1, '2024-02-12', "Shahra e faisal karachi", "Event of our owner"),
+( "Shayan's Marriage", 1, '2024-04-22', "Surjani town", "Event of our owner"),
+("Ali's Nikkah", 1, '2024-08-11', "Memon masjid", "Event of our best client"),
+("AI Seminar", 2, '2024-08-13', 'JDC IT City', 'Big names will be in event'),
+( "English Language Course", 2, '2024-08-14', 'PIDC', 'Nothing special');
