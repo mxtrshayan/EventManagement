b150ac5e9286073d070d990548a8d7b10ae292e2
Shayan <boloshayan@gmail.com>
2024-05-30 20:37:08 +0500

    constraints are added

--- Registrations.sql
+++ Registrations.sql
@@ -1,20 +1,19 @@
-CREATE TABLE Registrations (
-    registration_id INT PRIMARY KEY,
-    attendee_id INT,
-    event_id INT,
-    payment_status VARCHAR(50),
-    FOREIGN KEY (attendee_id) REFERENCES Attendees(attendee_id),
-    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+create table Registrations (
+    Registration_id int primary key auto_increment,
+    Attendee_id int not null,
+    Event_id int not null,
+    Payment_status varchar(50) not null,
+    foreign key (Attendee_id) references attendees(attendee_id),
+    foreign key (Event_id) references events(event_id)
 );
 
-insert into registrations(registration_id, attendee_id, event_id, payment_status)
-values 
-(1,8,4, 'yes'),
-(2,9,4,'yes'),
-(3,10,4, 'no'),
-(4,11,4,'yes'),
-(5,12,4, 'yes'),
-(6,13,5,'yes'),
-(7,14,5, 'no'),
-(8,15,5,'yes'),
-(9,7,5, 'yes');
+insert into registrations (attendee_id, event_id, payment_status) values
+(8, 4, 'yes'),
+(9, 4, 'yes'),
+(10, 4, 'no'),
+(11, 4, 'yes'),
+(12, 4, 'yes'),
+(13, 5, 'yes'),
+(14, 5, 'no'),
+(15, 5, 'yes'),
+(7, 5, 'yes');
