e728339acb3400d32dfba07b62de9569848bbab3
Shayan <boloshayan@gmail.com>
2024-05-30 20:36:17 +0500

    Foreign key and necessary constraints are added

--- Venues.sql
+++ Venues.sql
@@ -1,13 +1,15 @@
-CREATE TABLE Venues (
-    venue_id INT PRIMARY KEY,
-    venue_name VARCHAR(90),
-    address VARCHAR(95),
-    capacity INT
+create table Venues (
+    Venue_id int primary key auto_increment,
+    Event_id int not null,
+    Venue_name varchar(90) not null,
+    Address varchar(95) not null,
+    Capacity int not null,
+    foreign key (Event_id) references events(event_id)
 );
 
-insert into venues(venue_id, venue_name, address, capacity)
- values 
-(1,'Diamond banquet', 'Federal b area karachi', 400),
-(2,'Crystal lawn', 'UsmanAbad karachi', 500),
-(3,'Memon masjid', 'MA jinnah road karachi', 450),
-(4,'PACC', 'PIDC', 900);
+insert into venues (event_id, venue_name, address, capacity) values
+(1, 'Diamond banquet', 'Federal b area karachi', 400),
+(2, 'Crystal lawn', 'UsmanAbad karachi', 500),
+(3, 'Memon masjid', 'MA jinnah road karachi', 450),
+(4, 'PACC', 'PIDC', 900);
+
