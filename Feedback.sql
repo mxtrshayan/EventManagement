38cc30abc35e9fc28a008c1cf624653a741a4dd0
Shayan <boloshayan@gmail.com>
2024-05-30 20:39:38 +0500

    Constraints are added

--- Feedback.sql
+++ Feedback.sql
@@ -1,18 +1,18 @@
-CREATE TABLE Feedback (
-    feedback_id INT PRIMARY KEY,
-    event_id INT,
-    attendee_id INT,
-    rating INT,
-    comments TEXT,
-    FOREIGN KEY (event_id) REFERENCES Events(event_id),
-    FOREIGN KEY (attendee_id) REFERENCES Attendees(attendee_id)
+create table Feedback (
+    Feedback_id int primary key auto_increment,
+    Event_id int not null,
+    Attendee_id int not null,
+    Rating float not null check (rating >= 1 and rating <= 5),
+    Comments text,
+    foreign key (Event_id) references events(event_id),
+    foreign key (Attendee_id) references attendees(attendee_id)
 );
 
-insert into Feedback(feedback_id , event_id, attendee_id, rating, comments)
-values(1, 1, 8, 10, 'Dinner was amazing'),
-(2, 1, 9, 3, 'lighting was not good'),
-(3, 2, 10, 9, 'Dance was good'),
-(4, 2, 7, 10, 'Songs were amazing'),
-(5, 4, 5, 10, 'Got rare knowledge about automation'),
-(6, 4, 4, 1, 'Poor experience ever'),
-(7, 5, 3, 7, 'Avg experience');
+insert into feedback(event_id, attendee_id, rating, comments)
+values(1, 8, 5, 'Dinner was amazing'),
+(1, 9, 3, 'lighting was not good'),
+(2, 10, 4.5, 'Dance was good'),
+(2, 7, 5, 'Songs were amazing'),
+(4, 5, 5, 'Got rare knowledge about automation'),
+(4, 4, 1, 'Poor experience ever'),
+(5, 3, 3, 'Avg experience');
