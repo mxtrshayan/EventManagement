462c31da0b708ed1de499e6e734c3a715c8437e5
Shayan <boloshayan@gmail.com>
2024-05-30 20:34:40 +0500

    Constraints are added

--- Speakers.sql
+++ Speakers.sql
@@ -1,12 +1,12 @@
-CREATE TABLE Speakers (
-    speaker_id INT PRIMARY KEY,
-    event_id INT,
-    name VARCHAR(75),
-    bio TEXT,
-    contact_info VARCHAR(55),
-    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+create table Speakers (
+    Speaker_id int primary key auto_increment,
+    Event_id int not null,
+    Name varchar(55) not null,
+    Bio text not null,
+    Contact_info varchar(75) not null,
+    foreign key (Event_id) references events(event_id)
 );
 
-insert into speakers values
-(1, 1, 'Atif Aslam', 'Well known singer in Pakistan', 'intagram @atifaslam'),
-(2, 1, 'Chahat Fateh Ali Khan', 'Instagram singer', 'intagram @chahatAlikhan');
+insert into speakers (event_id, name, bio, contact_info) values
+( 1, 'Atif Aslam', 'Well known singer in Pakistan', 'instagram @atifaslam'),
+(1, 'Chahat Fateh Ali Khan', 'Instagram singer', 'instagram @chahatAlikhan');
